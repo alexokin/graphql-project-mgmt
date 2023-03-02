5ef69383d727af483817f544b64693914fe9fad7
alexokin <alex.okin@gmail.com>
2023-03-02 13:14:15 +0200

    small env change

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema')
 const connectDB = require('./config/db')
 
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -18,6 +19,6 @@ connectDB()
 app.use(cors())
 app.use('/graphql', graphqlHTTP({
     schema, 
-    graphiql: process.env.NODE_ENV === 'development',
+    graphiql: process.env.NODE_ENV === 'production',
 }))
 app.listen(port, console.log(`Server is running on port ${port}`));
