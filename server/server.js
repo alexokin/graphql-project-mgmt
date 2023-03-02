8fa94141da16a6e481583de7d3ca36708504ff62
alexokin <alex.okin@gmail.com>
2023-03-02 13:38:40 +0200

    adding path and changing cors config

--- server/server.js
+++ server/server.js
@@ -1,24 +1,38 @@
 const express = require("express");
-const colors = require('colors')
-const cors = require('cors')
-require("dotenv").config();
-const {graphqlHTTP} = require('express-graphql');
-const schema = require('./schema/schema')
-const connectDB = require('./config/db')
+const colors = require("colors");
+const cors = require("cors");
+const path = require("path");
 
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:5000', 'http://localhost:5000'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
+
 // Connect to database
-connectDB()
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "production",
+  })
+);
 
 
 
-app.use(cors())
-app.use('/graphql', graphqlHTTP({
-    schema, 
-    graphiql: process.env.NODE_ENV === 'production',
-}))
 app.listen(port, console.log(`Server is running on port ${port}`));
