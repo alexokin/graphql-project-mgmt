ad919e4a6ebfe0a4774f34eec967dd63656aadd9
alexokin <alex.okin@gmail.com>
2023-03-02 10:41:02 +0200

    env and db changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
-}
+  } catch (err) {
+    console.error(`Error: ${err.message}`.red.underline.bold);
+    process.exit(1);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
